d3f6b2a46f20a921638fbad41be40aa728e53237
FIREPC\FIRE <firebenderyt@gmail.com>
2023-01-16 21:17:39 +0200

    init

---
+++ wikiracing.py
@@ -0,0 +1,11 @@
+from typing import List
+
+requests_per_minute = 100
+links_per_page = 200
+
+
+class WikiRacer:
+
+    def find_path(self, start: str, finish: str) -> List[str]:
+        # implementation goes here
+        pass
